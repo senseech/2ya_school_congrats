7505121477ab03b1ae55e1624e71d1bdc7ea1f94
senseech <maxic1234@gmail.com>
2022-05-24 19:29:08 +0300

    Initial commit

---
+++ src/main/java/ya_ch2_gc.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ya_ch2_gc {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        //Количество вакансий
+        int number_job = in.nextInt();
+        ArrayList<Type_job> arrayList_job = new ArrayList<Type_job>();
+
+        for (int i = 0; i < number_job; i++){
+            String str = in.next();
+            String[] split_str = (str.split(","));
+            Type_job type_job = new Type_job(split_str[0],Integer.parseInt(split_str[1]));
+            arrayList_job.add(type_job);
+        }
+
+        int number_worker = in.nextInt();
+        ArrayList<Type_worker> arrayList_worker = new ArrayList<Type_worker>();
+
+        for (int i = 0; i < number_worker; i++){
+            String str = in.next();
+            String[] split_str = str.split(",");
+            Type_worker type_worker = new Type_worker(split_str[0], split_str[1],
+                    Integer.parseInt(split_str[2]), Integer.parseInt(split_str[3]));
+            arrayList_worker.add(type_worker);
+        }
+
+//        НИКАКАЯ СОРТИИРОВКА
+        ArrayList sorted_workers = new ArrayList<>();
+
+        for (int i = 0; i < number_worker; i++){
+            arrayList_worker.get(i).getType_job();
+        }
+
+//        arrayList_job.sort();
+//        Collections.sort(arrayList_job(0).);
+//        arrayList_job.get(0);
+
+        System.out.println();
+    }
+}
+
+class Type_job {
+    String name_job;
+    int count_worker;
+
+    public Type_job(String name_job, int count_worker) {
+        this.name_job = name_job;
+        this.count_worker = count_worker;
+    }
+
+    public String getName_job() {
+        return name_job;
+    }
+
+    public void setName_job(String name_job) {
+        this.name_job = name_job;
+    }
+
+    public int getCount_worker() {
+        return count_worker;
+    }
+
+    public void setCount_worker(int count_worker) {
+        this.count_worker = count_worker;
+    }
+}
+class Type_worker{
+    String name_worker;
+    String type_job;
+    int count_task;
+    int count_mistake;
+
+    public Type_worker(String name_worker, String type_job, int count_task, int count_mistake) {
+        this.name_worker = name_worker;
+        this.type_job = type_job;
+        this.count_task = count_task;
+        this.count_mistake = count_mistake;
+    }
+
+    public String getName_worker() {
+        return name_worker;
+    }
+
+    public void setName_worker(String name_worker) {
+        this.name_worker = name_worker;
+    }
+
+    public String getType_job() {
+        return type_job;
+    }
+
+    public void setType_job(String type_job) {
+        this.type_job = type_job;
+    }
+
+    public int getCount_task() {
+        return count_task;
+    }
+
+    public void setCount_task(int count_task) {
+        this.count_task = count_task;
+    }
+
+    public int getCount_mistake() {
+        return count_mistake;
+    }
+
+    public void setCount_mistake(int count_mistake) {
+        this.count_mistake = count_mistake;
+    }
+}
